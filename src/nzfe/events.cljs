857948e3ad86066cc5293af68ad10ace5bb0d70a
Art Goldhammer <art.goldhammer@gmail.com>
2022-03-14 11:02:56 -0400

    cat fetch working

--- src/nzfe/events.cljs
+++ src/nzfe/events.cljs
@@ -1,8 +1,16 @@
 (ns nzfe.events
   (:require
+   [clojure.string :as string]
    [re-frame.core :as re-frame]
    [nzfe.db :as db]
+   [day8.re-frame.http-fx]
+   [ajax.core :as ajax]
+   [tick.core :as t]
    [day8.re-frame.tracing :refer-macros [fn-traced]]))
+
+;; TODO Fix this later with tick
+(def now-as-string "2022-03-15")
+(println "tnow is " (t/now))
 
 (re-frame/reg-event-db
  ::initialize-db
@@ -25,4 +33,232 @@
  ::set-active-tab
  (fn [db [_ tab-id]]
    (assoc-in db [:tabs :active] tab-id)))
+
+(re-frame/reg-event-db
+ ::ajax-error
+ (fn [db [_ details]]
+   (re-frame/dispatch [:alert (:status-text details)])
+   (.log js/console details)
+   db))
+
+(re-frame/reg-event-db
+ ::got-cats
+ (fn [db [_ result]]
+   (->
+    db
+    (assoc :cats-loading? false)
+    (assoc :navdata result))))
+
+(re-frame/reg-event-fx
+ ::get-cats
+ (fn [{:keys [db]} _]
+   {:db (assoc db :cats-loading? true)
+    :http-xhrio {:method :get
+                 :uri "/json/cats"
+                 :timeout 10000
+                 :response-format
+                 (ajax/json-response-format {:keywords? true})
+                 :on-success [::got-cats]
+                 :on-failure [::ajax-error]}}))
+
+(comment
+  (re-frame/dispatch [::get-cats])
+
+  (re-frame/reg-event-db
+   ::alert
+   (fn [db [_ msg]]
+     (assoc db :alert msg)))
+
+  (re-frame/reg-event-db
+   ::got-count
+   (fn [db [_ result]]
+     (->
+      db
+      (assoc :time-of-count (now-as-string))
+      (assoc :cats-loading? false)
+      (assoc :count (:count result)))))
+
+
+;; auxiliary function to set up author-display-state section of db
+  (defn set-author-display-states
+    "Given a seq of statuses, return map {auth: true} with key for each author"
+    [statuses]
+    (let [authors (distinct (map
+                             (comp string/upper-case :author) statuses))]
+      (zipmap authors (repeat true))))
+
+
+  (re-frame/reg-event-db
+   ::toggle-author-display-state
+   (fn [db [_ author state]]
+     (assoc-in db [:author-display-states author] state)))
+
+
+  (re-frame/reg-event-db
+   ::set-display-all-authors-flag
+   (fn [db [_ true-or-false]]
+     (assoc db :display-all-authors? true-or-false)))
+
+
+  (re-frame/reg-event-db
+   ::set-reset-author-display-states
+   (fn [db [_ true-or-false]]
+     (let [authors (keys (:author-display-states db))]
+       (->
+        db
+        (assoc :display-all-authors? true-or-false)
+        (assoc :author-display-states
+               (zipmap authors (repeat true-or-false)))))))
+
+
+  (re-frame/reg-event-db
+   ::got-recent
+   (fn [db [_ result]]
+     (when (empty? result) (re-frame/dispatch [:alert "Server returned nothing"]))
+     (re-frame/dispatch [:reset-content-scroll-pos])
+     (re-frame/dispatch [:set-display-all-authors-flag true])
+     (->
+      db
+      (assoc :author-display-states (set-author-display-states result))
+      (assoc :recent-loading? false)
+      (assoc :recent result))))
+
+
+
+
+
+  (re-frame/reg-event-fx
+   ::get-count
+   (fn [{:keys [_]} _]
+     {:http-xhrio {:method :get
+                   :uri "/json/count"
+                   :timeout 10000
+                   :response-format
+                   (ajax/json-response-format {:keywords? true})
+                   :on-success [:got-count]
+                   :on-failure [:ajax-error]}}))
+
+
+
+  (re-frame/reg-event-fx
+   ::get-recent
+   (fn [{:keys [db]} _]
+     {:db (-> db
+              (assoc :recent-loading? true)
+              (assoc :display "Latest!"))
+      :http-xhrio {:method :get
+                   :uri "/json/recent"
+                   :timeout 10000
+                   :format (ajax/url-request-format :java)
+                   :response-format
+                   (ajax/json-response-format {:keywords? true})
+                   :on-success [:got-recent]
+                   :on-failure [:ajax-error]}}))
+
+
+  (re-frame/reg-event-fx
+   ::get-query
+   (fn [{:keys [db]} [_ query]]
+     {:db (assoc db :recent-loading? true)
+      :http-xhrio {:method :get
+                   :uri "/json/qry"
+                   :timeout 30000
+                   :format (ajax/url-request-format :java)
+                   :params {:data query}
+                   :response-format
+                   (ajax/json-response-format {:keywords? true})
+                   :on-success [:got-recent]
+                   :on-failure [:ajax-error]}}))
+
+
+  (re-frame/reg-event-db
+   ::initialize-db
+   (fn  [_ _]
+     db/default-db))
+
+
+  (re-frame/reg-event-db
+   ::initialize-content
+   (fn [db _]
+   ;; set timer to refresh stats every 10 mins
+     #_(js/setInterval #(re-frame/dispatch [:get-count]) 60000)
+     (re-frame/dispatch [:get-count])
+     (re-frame/dispatch [:get-cats])
+     (re-frame/dispatch [:get-recent])
+     db))
+
+
+  (re-frame/reg-event-db
+   ::set-display
+   (fn [db [_ display]]
+     (assoc db :display display)))
+
+
+  (re-frame/reg-event-db
+   ::topic-req
+   (fn [db [_ topic]]
+     (let [time-part @(re-frame/subscribe [:query-time])
+           query (str time-part " *" topic)]
+       (re-frame/dispatch [:set-display query])
+       (re-frame/dispatch [:get-query query]))
+     db))
+
+
+  (re-frame/reg-event-db
+   ::category-req
+   (fn [db [_ category]]
+     (let [time-part @(re-frame/subscribe [:query-time])
+           topics    @(re-frame/subscribe [:topics-by-category category])
+           text-part (string/join (map #(str " *" %1) topics))]
+       (re-frame/dispatch [:set-display (string/join " " [time-part text-part])])
+       (re-frame/dispatch [:get-query (string/join " " [time-part text-part])]))
+     db))
+
+
+;; must quote query text to accommodate multiple search terms
+  (re-frame/reg-event-db
+   :custom-query-req
+   (fn [db [_ text]]
+     (let [time-part @(re-frame/subscribe [:query-time])]
+       (re-frame/dispatch [:get-query (string/join " " [time-part text])]))
+     db))
+
+
+  (re-frame/reg-event-db
+   ::set-active-time-button
+   (fn [db [_ activate-id]]
+     (when (= activate-id :tb6) (re-frame/dispatch [:toggle-show-custom-time-panel]))
+     (assoc-in db [:time-button-bar :active] activate-id)))
+
+
+  (re-frame/reg-event-db
+   ::set-custom-query
+   (fn [db [_ text]]
+     (assoc-in db [:custom-query :text] text)))
+
+
+  (re-frame/reg-event-db
+   ::set-custom-query-status
+   (fn [db [_ status]]
+     (assoc-in db [:custom-query :status] status)))
+
+
+  (re-frame/reg-event-db
+   ::toggle-show-custom-time-panel
+   (fn [db]
+     (update db :show-custom-time-panel? not)))
+
+
+  (re-frame/reg-event-db
+   ::set-custom-date
+   (fn [db [_ start-or-end date]]
+     (assoc-in db [:custom-date start-or-end] date)))
+
+
+  (re-frame/reg-event-db
+   ::reset-content-scroll-pos
+   (fn [db]
+     (let [content (.getElementById js/document "content1")]
+       (aset content "scrollTop" 0))
+     db)))
 
--- src/nzfe/subs.cljs
+++ src/nzfe/subs.cljs
@@ -24,115 +24,6 @@
    (nth (get-in db [:time-button-bar :ids button-id]) 0)))
 
 (re-frame/reg-sub
- ::item-count
- (fn [db]
-   (:count db)))
-
-(re-frame/reg-sub
- ::get-time-of-count
- (fn [db]
-   (:time-of-count db)))
-
-(re-frame/reg-sub
- ::cats-loading?
- (fn [db]
-   (:cats-loading? db)))
-
-(re-frame/reg-sub
- ::recent-loading?
- (fn [db]
-   (:recent-loading? db)))
-
-(re-frame/reg-sub
- ::categories
- (fn [db]
-   (keys (get-in db [:navdata :cats]))))
-
-(re-frame/reg-sub
- ::category
- (fn [db [_ category]]
-   (get-in db [:navdata :cats category])))
-
-(re-frame/reg-sub
- ::topics-by-category
- (fn [db [_ category]]
-   (mapv #(:topic %1) (get-in db [:navdata :cats category]))))
-
-(re-frame/reg-sub
- ::topic-descs-by-category
- (fn [db [_ category]]
-   (mapv #((juxt :topic :desc) %1) (get-in db [:navdata :cats category]))))
-
-(re-frame/reg-sub
- ::fulltopic
- (fn [db [_ category topic]]
-   (let [topics (get-in db [:navdata :cats category])]
-     (first (filter #(= topic (:topic %1)) topics)))))
-
-(re-frame/reg-sub
- ::topic-to-query
- (fn [db [_ category topic]]
-   (:query @(re-frame/subscribe [:fulltopic category topic]))))
-
-(re-frame/reg-sub
- ::get-display-text
- (fn [db]
-   (:display db)))
-
-;; this section is for testing
-(defn fake-status-list
-  [n db]
-  (take n (repeat (:dummy-list db))))
-
-(re-frame/reg-sub
- ::get-authors
- (fn [db]
-   (sort (keys @(re-frame/subscribe [:get-author-display-states])))))
-
-(re-frame/reg-sub
- ::get-author-display-states
- (fn [db]
-   (:author-display-states db)))
-
-(re-frame/reg-sub
- ::get-author-display-state
- (fn [db [_ author]]
-   (get-in db [:author-display-states author])))
-
-(re-frame/reg-sub
- ::get-fake-status-list
- (fn [db [_ n]]
-   (fake-status-list n db)))
-;;;;;;;
-
-(re-frame/reg-sub
- ::get-active-authors
- (fn [db]
-   (into #{}
-         (map first (filter second (:author-display-states db))))))
-
-(re-frame/reg-sub
- ::get-recent
- (fn [db]
-   (:recent db)))
-
-(defn status-author-active?
-  [status active-authors]
-  (some #{(string/upper-case (:author status))} active-authors))
-
-(re-frame/reg-sub
- ::filtered-statuses
- (fn [db]
-   (let [active-authors @(re-frame/subscribe [:get-active-authors])
-         statuses (:recent db)]
-     (filterv #(status-author-active? %1 active-authors) statuses))))
-
-(re-frame/reg-sub
- ::display-all-authors?
- (fn [db]
-   (:display-all-authors? db)))
-
-(re-frame/reg-sub
  ::time-dd-active?
  (fn [db]
    (-> db :time-dd :active?)))
@@ -141,3 +32,113 @@
  ::get-active-tab
  (fn [db]
    (-> db (get-in [:tabs :active]))))
+
+(comment
+  (re-frame/reg-sub
+   ::item-count
+   (fn [db]
+     (:count db)))
+
+  (re-frame/reg-sub
+   ::get-time-of-count
+   (fn [db]
+     (:time-of-count db)))
+
+  (re-frame/reg-sub
+   ::cats-loading?
+   (fn [db]
+     (:cats-loading? db)))
+
+  (re-frame/reg-sub
+   ::recent-loading?
+   (fn [db]
+     (:recent-loading? db)))
+
+  (re-frame/reg-sub
+   ::categories
+   (fn [db]
+     (keys (get-in db [:navdata :cats]))))
+
+  (re-frame/reg-sub
+   ::category
+   (fn [db [_ category]]
+     (get-in db [:navdata :cats category])))
+
+  (re-frame/reg-sub
+   ::topics-by-category
+   (fn [db [_ category]]
+     (mapv #(:topic %1) (get-in db [:navdata :cats category]))))
+
+  (re-frame/reg-sub
+   ::topic-descs-by-category
+   (fn [db [_ category]]
+     (mapv #((juxt :topic :desc) %1) (get-in db [:navdata :cats category]))))
+
+  (re-frame/reg-sub
+   ::fulltopic
+   (fn [db [_ category topic]]
+     (let [topics (get-in db [:navdata :cats category])]
+       (first (filter #(= topic (:topic %1)) topics)))))
+
+  (re-frame/reg-sub
+   ::topic-to-query
+   (fn [_ [_ category topic]]
+     (:query @(re-frame/subscribe [:fulltopic category topic]))))
+
+  (re-frame/reg-sub
+   ::get-display-text
+   (fn [db]
+     (:display db)))
+
+;; this section is for testing
+  (defn fake-status-list
+    [n db]
+    (take n (repeat (:dummy-list db))))
+
+  (re-frame/reg-sub
+   ::get-authors
+   (fn [_]
+     (sort (keys @(re-frame/subscribe [:get-author-display-states])))))
+
+  (re-frame/reg-sub
+   ::get-author-display-states
+   (fn [db]
+     (:author-display-states db)))
+
+  (re-frame/reg-sub
+   ::get-author-display-state
+   (fn [db [_ author]]
+     (get-in db [:author-display-states author])))
+
+  (re-frame/reg-sub
+   ::get-fake-status-list
+   (fn [db [_ n]]
+     (fake-status-list n db)))
+;;;;;;;
+
+  (re-frame/reg-sub
+   ::get-active-authors
+   (fn [db]
+     (into #{}
+           (map first (filter second (:author-display-states db))))))
+
+  (re-frame/reg-sub
+   ::get-recent
+   (fn [db]
+     (:recent db)))
+
+  (defn status-author-active?
+    [status active-authors]
+    (some #{(string/upper-case (:author status))} active-authors))
+
+  (re-frame/reg-sub
+   ::filtered-statuses
+   (fn [db]
+     (let [active-authors @(re-frame/subscribe [:get-active-authors])
+           statuses (:recent db)]
+       (filterv #(status-author-active? %1 active-authors) statuses))))
+
+  (re-frame/reg-sub
+   ::display-all-authors?
+   (fn [db]
+     (:display-all-authors? db))))
