2ca11bb7572067ce56ae132c6f28bae68749f322
Art Goldhammer <art.goldhammer@gmail.com>
2022-03-28 09:24:25 -0400

    mod deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-cp resources/public/js/compiled/app.js ../nooze2/app/js/
+echo "copying files to nooze2/app"
+cp resources/public/js/compiled/app.js ../nooze2/app/static/js/
 cp resources/public/static/nzfe.css ../nooze2/app/static/css/
 
 
